90f4dcf11e9394df6869079d6a92b6f3ea7c15af
Leonardo Mota Leite <leomotaleit@gmail.com>
2024-09-12 12:20:21 -0300

    fndmts 5

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const start = () => {
-    let contador = 1
-    while(contador <= 10){
-        console.log(contador)
-        contador ++
+    
+    while(true){
+        let opcao = "sair"
+        switch(opcao){
+            case "cadastro":
+                console.log("vc está se cadastrando")
+                break
+            case "listar":
+                console.log("vc está listando")
+                break
+            case "sair":
+                return
+        }
     }
 }
+
 start()
